1bc790dbb0d434af963b6811dfcebe1902cf0e01
AnfasArshad <anfasarshad@gmail.com>
2026-07-26 21:27:39 +0530

    Trigger redeploy with file changed

--- src/components/Travels.jsx
+++ src/components/Travels.jsx
@@ -120,7 +120,7 @@ export default function Travels() {
                     <option value="1">1 Guest</option>
                     <option value="2">2 Guests</option>
                     <option value="4">4 Guests</option>
-                    <option value="6">8+ Guests</option>
+                    <option value="6">6+ Guests</option>
                   </select>
                 </div>
               </div>
